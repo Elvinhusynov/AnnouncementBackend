679414187179271d525e768c920e7eb1e1d281ec
Elvinhusynov <huseynov044@gmail.com>
2025-02-26 11:37:33 +0400

    use validation

--- src/main/java/com/huseynov/announcementbackend/dto/CreateAnnouncementRequest.java
+++ src/main/java/com/huseynov/announcementbackend/dto/CreateAnnouncementRequest.java
@@ -1,19 +1,30 @@
 package com.huseynov.announcementbackend.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class CreateAnnouncementRequest {
-    @NotNull(message = "Name can not be null")
+
+    @NotBlank(message = "Name can not be blank")
     private String name;
     private String description;
+
     @NotNull(message = "Price can not be null")
+    @Min(value = 0, message = "Price must be greater than or equal to zero")
     private Double price;
+
     @NotNull(message = "phone number can not be null")
+    @Size(min = 10 , max = 10 , message = "Phone number must contains 10 characters")
+    @Pattern(regexp = "//d{10}" , message = "Phone number must contain only digits")
     private String phoneNumber;
+
     private String sellerFullName;
     private Boolean delivery;
+
+    @NotNull(message = "City id can not be null")
     private Long cityId;
+
+    @NotNull(message = "Category id can not be null")
     private Long categoryId;
 }
--- src/main/java/com/huseynov/announcementbackend/dto/UpdateAnnouncementRequest.java
+++ src/main/java/com/huseynov/announcementbackend/dto/UpdateAnnouncementRequest.java
@@ -1,11 +1,18 @@
 package com.huseynov.announcementbackend.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UpdateAnnouncementRequest {
+    @NotBlank(message = "Name can not be blank")
     private String name;
     private String description;
+
+    @NotNull(message = "Price can not be null")
+    @Min(value = 0, message = "Price must be greater than or equal to zero")
     private Double price;
     private String sellerFullName;
     private Boolean delivery;
